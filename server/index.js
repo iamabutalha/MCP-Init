9db86caed56a20c5342b5088f2953a35ac7660b4
iamabutalha <maabutalhakhalil@gmail.com>
2026-03-14 10:08:04 +0500

    add prompts and resources

--- server/index.js
+++ server/index.js
@@ -1,7 +1,8 @@
-import {McpServer} from "@modelcontextprotocol/sdk/server/mcp.js"
+import {McpServer, ResourceTemplate} from "@modelcontextprotocol/sdk/server/mcp.js"
 import {StdioServerTransport} from '@modelcontextprotocol/sdk/server/stdio.js'
 import {z} from "zod"
 import fs from "node:fs/promises"
+import { CreateMessageResultSchema } from "@modelcontextprotocol/sdk/types.js"
 const server = new McpServer({
     name: "test",
     version: "1.0.0",
@@ -12,6 +13,54 @@ const server = new McpServer({
 
     }
 })
+
+server.resource(
+    "users",
+    "user://all",
+    {
+        description: "Get all user data from database",
+        title: "Users",
+        mimeType: "application/json"
+    }, async uri => {
+
+        const users = await import("./data/user.json", {
+            with: {type: "json"}
+        }).then(m => m.default)
+
+        return {
+            contents: [{ uri: uri.href, text: JSON.stringify(users), mimeType:  "application/json"}]
+        }
+    }
+)
+
+server.resource("user-details", new ResourceTemplate("user://{userId}/profile", {
+    list: undefined
+}),   {
+        description: "Get a user details from database",
+        title: "Users",
+        mimeType: "application/json"
+    }, async (uri, {userId}) => {
+        const users = await import("./data/user.json", {
+            with: {type: "json"}
+        }).then(m => m.default)
+
+        const user = users.find(u => u.id === parseInt(userId))
+
+        if(user == null) {
+            return {
+                contents: [
+                    {
+                        uri: uri.href, text: JSON.stringify({error: "User not found"}),
+                        mimeType: "application/json"
+                    }
+                ]
+            }
+        }
+
+        return {
+            contents: [{ uri: uri.href, text: JSON.stringify(user), mimeType:  "application/json"}]
+        }
+    })
 
 server.tool('create-user', "create a new user in the database", {
     name: z.string(),
@@ -45,6 +94,68 @@ server.tool('create-user', "create a new user in the database", {
         }
      }
 
+})
+
+server.tool("create-random-user", "Create a random user with fake data", {
+    title: "Create user",
+    readOnlyHint: false,
+    destructiveHint: false,
+    idempotentHint: false,
+    openWorldHint: true
+}, async () => {
+    // using my own prompt function i created
+   const res = await server.server.request({
+        method: "sampling/createMessage",
+        params: {
+            messages: [{
+                role: "user",
+                content: {
+                    type: "text",
+                    text: "Generate a fake user data. The user should have a realsitic name, email, address and phone number. Return this data as a JSON object with no other text or formater so it can be used with JSON.parse having an id also"
+                }
+            }],
+            maxTokens: 1024
+        }
+    }, CreateMessageResultSchema)
+
+    if (res.content.type !== "text") {
+        return {
+            content: [{type: "text", text: "Failed to generate user data"}]
+        }
+    }
+
+    try {
+        const fakeUser = JSON.parse(res.content.text.trim().replace(/^```json/, "").replace(/```$/, "")).trim()
+
+        const id = await createUser(fakeUser)
+
+        return {
+            content:[{
+                type: "text", text: `User ${id} created successfully`
+            }]
+        }
+    } catch (error) {
+        return {
+            content: [{
+                type: "text", text: "Failed to generate user data",
+                error
+            }]
+        }
+    }
+})
+
+server.prompt("generate-fake-user", "Generate a fake user based on a given name", {
+    name: z.string(),
+
+}, ({name}) => {
+    return {
+        messages: [{
+            role: 'user',
+            content: {
+                type: "text", text: `Generate a fake user with the name ${name}. The user should have a relastic email, address and phone number.`
+            }
+        }]
+    } 
 })
 
 async function createUser(user) {
